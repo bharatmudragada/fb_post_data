7d672b1687dfab334e81a98bc4fc8b1228289ed3
Bharat Mudragada <bharathmudragada123@gmail.com>
2019-07-20 16:46:22 +0530

    added tests to get user posts interactor

---
+++ fb_post_v2/tests/interactors/test_get_user_posts_interactor.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import create_autospec
+
+from fb_post_v2.interactors.get_user_posts_interactor import GetUserPostsInteractor
+from fb_post_v2.interactors.presenters.json_presenter import JsonPresenter
+from fb_post_v2.interactors.storages.post_storage import PostStorage, GetUserPostsDTO
+
+
+class TestGetUserPosts(unittest.TestCase):
+
+    def test_get_user_posts(self):
+        post_storage_mock = create_autospec(PostStorage)
+        presenter_mock = create_autospec(JsonPresenter)
+
+        user_id = 1
+        response_data = {"posts": "post_details"}
+
+        user_posts_dto = create_autospec(GetUserPostsDTO)
+        post_storage_mock.get_user_posts.return_value = user_posts_dto
+        presenter_mock.get_user_posts_response.return_value = response_data
+
+        get_user_posts_interactor = GetUserPostsInteractor(post_storage_mock, presenter_mock)
+        response = get_user_posts_interactor.get_user_posts(user_id)
+
+        post_storage_mock.get_user_posts.assert_called_once_with(user_id)
+        presenter_mock.get_user_posts_response.assert_called_once_with(user_posts_dto)
+        assert response == response_data
