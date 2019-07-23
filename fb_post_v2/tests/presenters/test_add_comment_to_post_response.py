b92cecbae39259a0b7490067e669cdce958bbd03
Bharat Mudragada <bharathmudragada123@gmail.com>
2019-07-23 13:04:16 +0530

    Added test to add comment to post presenter

---
+++ fb_post_v2/tests/presenters/test_add_comment_to_post_response.py
@@ -0,0 +1,20 @@
+import pytest
+from freezegun import freeze_time
+
+from fb_post_v2.interactors.storages.post_storage import CommentDTO
+from datetime import datetime
+
+from fb_post_v2.presenters.json_presenter import JsonPresenter
+
+
+class TestAddCommentToPostResponse:
+
+    @freeze_time("2019-08-18")
+    def test_add_comment_to_post(self):
+
+        comment_dto = CommentDTO(comment_id=1, user_id=1, commented_at=datetime.now(), comment_content="This is a comment", commented_on_id=None)
+
+        json_presenter = JsonPresenter()
+        response = json_presenter.get_add_comment_to_post_response(comment_dto)
+
+        assert response["comment_id"] == comment_dto.comment_id
