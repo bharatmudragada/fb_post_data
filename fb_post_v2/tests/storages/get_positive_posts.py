e699468e228e1893dd79dbd8f03e4d52a1758b27
Bharat Mudragada <bharathmudragada123@gmail.com>
2019-07-23 09:57:18 +0530

    added tests to get reactions to post and get reaction metrics in storages

---
+++ fb_post_v2/tests/storages/get_positive_posts.py
@@ -0,0 +1,28 @@
+import pytest
+from freezegun import freeze_time
+
+from fb_post_v2.storages.post_storage import PostStorage
+from fb_post_v2.models.models import *
+
+
+class TestGetPositivePosts:
+
+    @pytest.fixture
+    def setup_data(self):
+        self.user_1 = User.objects.create(username="user_1", profile_pic_url="https://user_1")
+        self.post = Post.objects.create(user=self.user_1, post_content="This is new post")
+        self.post_2 = Post.objects.create(user=self.user_1, post_content="This is another post")
+        self.post_reaction_1 = PostReactions.objects.create(post=self.post, user=self.user_1, reaction_type="LOVE")
+        self.post_reaction_2 = PostReactions.objects.create(post=self.post_2, user=self.user_1, reaction_type="SAD")
+
+    @pytest.mark.django_db
+    @freeze_time("2019-08-18")
+    def test_get_positive_posts_response(self, setup_data):
+
+        post_storage_object = PostStorage()
+        post_ids_dto = post_storage_object.get_positive_posts()
+
+        post_ids = post_ids_dto.post_ids
+
+        assert self.post.id in post_ids
+        assert self.post_2.id in post_ids
---
+++ fb_post_v2/tests/storages/test_get_reactions_to_post.py
@@ -0,0 +1,32 @@
+import pytest
+from freezegun import freeze_time
+
+from fb_post_v2.storages.post_storage import PostStorage
+from fb_post_v2.models.models import *
+
+
+class TestGetReactionsToPost:
+
+    @pytest.fixture
+    def setup_data(self):
+        self.user_1 = User.objects.create(username="user_1", profile_pic_url="https://user_1")
+        self.user_2 = User.objects.create(username="user_2", profile_pic_url="https://user_2")
+        self.post = Post.objects.create(user=self.user_1, post_content="This is new post")
+        self.post_reaction_1 = PostReactions.objects.create(post=self.post, user=self.user_1, reaction_type="LOVE")
+        self.post_reaction_2 = PostReactions.objects.create(post=self.post, user=self.user_2, reaction_type="SAD")
+
+    @pytest.mark.django_db
+    @freeze_time("2019-08-18")
+    def test_get_reactions_to_post(self, setup_data):
+
+        post_storage_object = PostStorage()
+        post_reactions_dto = post_storage_object.get_reactions_to_post(self.post.id, 0, 2)
+
+        reaction_one_data = None
+        for reaction in post_reactions_dto:
+            if reaction.user_id == self.user_1.id:
+                reaction_one_data = reaction
+
+        assert reaction_one_data.name == self.user_1.username
+        assert reaction_one_data.profile_pic_url == self.user_1.profile_pic_url
+        assert reaction_one_data.reaction_type == self.post_reaction_1.reaction_type
